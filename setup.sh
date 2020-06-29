34c3f66f078139fe2b439fca06f5c2f536784451
Manish Rawat <manish.rawat@flipkart.com>
2020-06-30 01:43:33 +0530

    package.json fix

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 sudo apt-get update
 sudo apt-get install npm -y
-cd ~
-git clone https://github.com/manishrawat1992/event-expo.git
+cd /home/manishrawat9992
+sudo git clone https://github.com/manishrawat1992/event-expo.git
 cd event-expo
 
 sudo npm install
